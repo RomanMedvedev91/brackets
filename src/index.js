b532d981cfc2d4775b03b513993de4868d769f0b
Roman Medvedev <roman.medvedev77@gmail.com>
2020-10-25 10:35:01 +0500

    init: initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    if (str.length === 0) return false;
+
+    for (i = 0; i < str.length; i++) {
+        for (j = 0; j < bracketsConfig.length; j++) {
+            if (
+                str[i] == bracketsConfig[j][0] &&
+                str[i + 1] == bracketsConfig[j][1]
+            ) {
+                str = str.replace(`${str[i]}${str[i + 1]}`, "");
+                i = i - 2;
+            }
+        }
+    }
+
+    return str === "";
+};
